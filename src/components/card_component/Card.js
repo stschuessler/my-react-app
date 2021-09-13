dd9066a0c1b781c36afff58b8d707d71871d961d
Sarah Schüßler <stschuessler@googlemail.com>
2021-09-13 15:39:14 +0200

    First entry and stylin Card component

--- src/components/card_component/Card.js
+++ src/components/card_component/Card.js
@@ -1,7 +1,14 @@
 import './Card.css'
 
 function Card() {
-  return <div>Card</div>
+  return (
+    <div className="card">
+      <h2 className="card_subtitle">Hermonie Granger</h2>
+      <p> I will become a picture</p>
+      <p> I will show you the house names</p>
+      <button>more</button>
+    </div>
+  )
 }
 
 export default Card
