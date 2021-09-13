c6c4a3dcf4347a07dbfef4c24eeee23182a33b1e
Sarah Schüßler <stschuessler@googlemail.com>
2021-09-13 21:42:31 +0200

    Add nav and button tags

--- src/components/footer_component/Footer.js
+++ src/components/footer_component/Footer.js
@@ -1,7 +1,16 @@
 import './Footer.css'
 
 function Footer() {
-  return <footer className="footer">Footer</footer>
+  return (
+    <footer className="footer">
+      <nav className="navigation">
+        <button className="navigation__button">Gryffindor</button>
+        <button className="navigation__button">Hufflepuff</button>
+        <button className="navigation__button">Ravenclaw</button>
+        <button className="navigation__button">Slytherin</button>
+      </nav>
+    </footer>
+  )
 }
 
 export default Footer
